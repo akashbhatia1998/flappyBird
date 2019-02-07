d25b0a1d9daa70ff96eb552c4cadeb7bc85f4831
AkashDeep Singh Bhatia <36357405+akashbhatia1998@users.noreply.github.com>
2019-02-07 12:13:48 +0530

    Update Renderer.java

--- Renderer.java
+++ Renderer.java
@@ -7,9 +7,6 @@ import javax.swing.JPanel;
 
 public class Renderer extends JPanel
 {
-
-	private static final long serialVersionUID = 1L;
-
 	@Override
 	protected void paintComponent(Graphics g)
 	{
